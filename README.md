6a32646f8297e01fc5a4b11b537a7fff3f290d28
Matheus Felipe dos Anjos <anjos97.matheus@gmail.com>
2021-09-25 04:09:35 -0300

    Readme wrote

--- README.md
+++ README.md
@@ -1,12 +1,83 @@
-Jupyter Notebook Container:
-``` console
-$ docker run -p 8888:8888 --name capstone_udacity_notebook -v /home/matheus/projects/udacity/data_engineering_nanodegree/udacity_data_engineering_capstone_project/:/src/notebook -v /home/matheus/projects/udacity/data_engineering_nanodegree/udacity_data_engineering_capstone_project/data/:/src/data -d anjosma/jupyter_env
-```
+# Capstone Project Udacity Data Engineering
+This is the last project of six from Udacity Nanodegree Data Engineering Program.
 
-Airflow:
-``` console
-$ mkdir ./dags ./logs ./plugins
-$ echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
-$ docker-compose up airflow-init
-$ docker-compose up
-```
+# Scope
+This project aims to prepare datasets for ELT (Extract, Load and Transform) in a Datalake using PySpark and Pandas to ensure data quality for all the information processed in the datasets. For this, we will use Jupyter Notebook to explore the datasets, PySpark to process high volume of data. The scripts written on this project has as the main goal creates a analytical visualization for the datasets to make data available for a Data Science Team for studies cases and to make it easier for Business Teams to create their own Business Intelligence Visualization.
+
+# Datasets
+The following datasets will be used on this project:
+- I94 Immigration Data: This data comes from the US National Tourism and Trade Office. A data dictionary is included in the workspace. This is where the data comes from. There's a sample file so you can take a look at the data in csv format before reading it all in. You do not have to use the entire dataset, just use what you need to accomplish the goal you set at the beginning of the project.
+- World Temperature Data: This dataset came from Kaggle and has a lot of information about temperature during period of a time.
+- U.S. City Demographic Data: This dataset contains information about the demographics of all US cities and census-designated places with a population greater or equal to 65,000.
+- Airport Code Table: It contains the list of all airport codes, the attributes are identified in datapackage description. Some of the columns contain attributes identifying airport locations, other codes (IATA, local if exist) that are relevant to identification of an airport.
+
+## Airport Data
+It contains the list of all airport codes, the attributes are identified in datapackage description. Some of the columns contain attributes identifying airport locations, other codes (IATA, local if exist) that are relevant to identification of an airport.
+
+### Dictionary
+- ident: Unique key for each row
+- type: Type of depart
+- name: Name of Depart place
+- elevation_ft:
+- continent: Name of the depart's continent
+- iso_country: Code of the country
+- iso_region: Code of the region (COUNTRY-STATE)
+- municipality: Name of the municipality depart
+- gps_code: Gps Code
+- iata_code: Iata Code
+- local_code: Local depart code
+- coordinates: Latitude and longitude of the depart place
+
+## Demographic Data
+This data comes from OpenSoft. This dataset contains information about the demographics of all US cities and census-designated places with a population greater or equal to 65,000. This data comes from the US Census Bureau's 2015 American Community Survey.
+
+### Dictionary
+- City: City name
+- State: USA State
+- Median Age: The median age from the city's population
+- Male Population: Number of Male people in the city
+- Female Population: Number of Female people in the city
+- Number of Veterans: Number of Veterans people in the city
+- Foreign-born: Number of people that were born in other cities
+- Average Household Size: Average number of people that lives in a household
+- State Code: Code of USA State
+- Race: People's race
+- Count: Number of people by race.
+
+
+## Temperatures Data
+This dataset came from Kaggle and has a lot of information about temperature during period of a time.
+
+### Dictionary
+- dt: Date reference
+- AverageTemperature: Global average land temperature in celsius
+- AverageTemperatureUncertainty: The 95% confidence interval around the average
+- City: Name of the city
+- Country: Name of the country
+- latitude: Latitude position
+- longitude: Longitude position
+
+
+## Immigration Data
+This data comes from the US National Tourism and Trade Office. A data dictionary is included in the workspace. This is where the data comes from. There's a sample file so you can take a look at the data in csv format before reading it all in. You do not have to use the entire dataset, just use what you need to accomplish the goal you set at the beginning of the project.
+
+# Explore and Access Data
+These steps are included in `notebook/Capstone Project.ipynb`.
+
+# Run the code
+The process is composed by two scripts:
+- `py_spark_job.py`: Execute the ELT process, gathering the data from all datasets, transforming and loading into `parquet` files.
+- `data_quality_checks.py`: Check if the output of the previously process were expected.
+
+# Project Write Up
+## What's the goal? What queries will you want to run? How would Spark or Airflow be incorporated? Why did you choose the model you chose?
+The goal of this model is to make an ELT model to make the RAW data available for not only data science but also business teams. With ELT model, the teams are free to develop their own rules with data, making the teams less dependents of data engineering teams.  
+Spark in EMR could be used for a huge volume of data and to make it available on cloud and Airflow can be used to schedule the process.
+
+## Propose how often the data should be updated and why.
+According to the data available on these datasets, some tables can be updated daily and others monthly. The update interval must be set up according to the necessity of the team.
+
+## Include a description of how you would approach the problem differently under the following scenarios:
+- **If the data was increased by 100x**: We can use AWS EMR to use parallel processing and make it possible to execute a huge volume of data.
+- **If the pipelines were run on a daily basis by 7am**: We can use Apache Airflow. With that we can schedule a lot of DAGs to make it available for a company. Also, in a company with ELT process, we can democratize the SQL and make the business people able to develop their only queries to be scheduled in Airflow.
+- **If the database needed to be accessed by 100+ people**: AWS Redshift fits well on this case. It's a cloud column partitioned database for analytics purposes.
--- scripts/py_spark_job.py
+++ scripts/py_spark_job.py
@@ -1,51 +1,152 @@
-#%%
 from pyspark.sql import SparkSession
 from pyspark.sql.types import DateType
-from pyspark.sql.functions import udf, col, lit, year, month, upper, to_date
-from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import udf, col, lit, year, month, upper, to_date, monotonically_increasing_id, split
+from pyspark.sql.types import StructField, StructType, StringType, IntegerType, DoubleType, LongType
 
 import sys, os
-sys.path.insert(0, '/'.join( os.getcwd().split("/")[:-1] ))
-from plugins.utils import load_yml_config
-
 import logging
 
+from utils import load_yml_config
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+os.environ["PATH"] = "/opt/conda/bin:/opt/spark-2.4.3-bin-hadoop2.7/bin:/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-8-openjdk-amd64/bin"
+os.environ["SPARK_HOME"] = "/opt/spark-2.4.3-bin-hadoop2.7"
+os.environ["HADOOP_HOME"] = "/opt/spark-2.4.3-bin-hadoop2.7"
+
 logging.info("Loading configuration files")
-credentials = load_yml_config(os.path.join("./", "config", "credentials.yml"))
 config = load_yml_config(os.path.join("./", "config", "etl.yml"))
 
-S3_BUCKET_IN = config["aws"]['s3']['input_bucket']
-S3_BUCKET_OUT = config["aws"]['s3']['output_bucket']
+PATH_OUT = config["data"]['output_bucket']
 
-IMMIGRATION_KEY = config["aws"]['s3']['immigration_data_key']
-AIRPORT_KEY = config["aws"]['s3']['airport_data_key']
-DEMOGRAPHICS_KEY = config["aws"]['s3']['demographics_data_key']
-
-logging.info("Setting AWS credentials")
-
-ACCESS_KEY = credentials['aws']["access_key"]
-SECRET_KEY = credentials['aws']["secret_key"]
-
-os.environ["AWS_ACCESS_KEY_ID"] = ACCESS_KEY
-os.environ["AWS_SECRET_KEY_ID"] = SECRET_KEY
+IMMIGRATION_KEY = config["data"]['immigration_data_key']
+AIRPORT_KEY = config['data']['airport_data_key']
+DEMOGRAPHICS_KEY = config['data']['demographics_data_key']
+TEMPERATURES_KEY = config["data"]['temperatures_data_key']
 
 os.environ['jdk.xml.entityExpansionLimit'] = '0' 
 #os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk-pom:1.11.538,org.apache.hadoop:hadoop-aws:2.7.4 pyspark-shell'
 
-#%%
 def prepare_demographic_data():
-#%%
-    demographic_data = os.path.join(S3_BUCKET_IN, DEMOGRAPHICS_KEY)
-    df = spark.read.format('csv').options(header=True, delimiter=";").load(demographic_data)
-    df.show()
+    """
+    Python function responsible to prepare the demographic data
+    """
+    
+    demographic_schema = StructType([
+        StructField('City', StringType()),
+        StructField('State', StringType()),
+        StructField('Median Age', DoubleType()),
+        StructField('Male Population', IntegerType()),
+        StructField('Female Population', IntegerType()),
+        StructField('Total Population', IntegerType()),
+        StructField('Number of Veterans', IntegerType()),
+        StructField('Foreign-born', IntegerType()),
+        StructField('Average Household Size', DoubleType()),
+        StructField('State Code', StringType()),
+        StructField('Race', StringType()),
+        StructField('Count', IntegerType()),
+    ])
+
+    demographic_data = DEMOGRAPHICS_KEY
+    df_demog = spark.read.format('csv').options(header=True, delimiter=";", ).schema(demographic_schema).load(demographic_data)
+    
+    logging.info("Number of rows in original demographic dataset: {}".format(df_demog.count()))
+    
+    # Make sure rows are unique 
+    df_demog_uni = df_demog.dropDuplicates(["Race", "City", "State"])
+    df_demog_renamed = df_demog_uni.withColumnRenamed('City', 'city')\
+        .withColumnRenamed('State', 'state')\
+        .withColumnRenamed('Median Age', 'median_age')\
+        .withColumnRenamed('Male Population', 'male_population')\
+        .withColumnRenamed('Female Population', 'female_population')\
+        .withColumnRenamed('Total Population', 'total_population')\
+        .withColumnRenamed('Number of Veterans', 'number_of_veterans')\
+        .withColumnRenamed('Foreign-born', 'number_of_foreign_born')\
+        .withColumnRenamed('Average Household Size', 'average_household_size')\
+        .withColumnRenamed('State Code', 'state_code')\
+        .withColumnRenamed('Race', 'race')\
+        .withColumnRenamed('Count', 'count')\
+        .withColumn('id_demog', monotonically_increasing_id())\
+        .withColumn('country', lit('United States'))
+    
+    logging.info("Number of rows in demographic dataset after drop duplicates: {}".format(df_demog_renamed.count()))
+    
+    file_name = demographic_data.split("/")[-1].split('.')[0]
+    df_demog_renamed.write.mode('overwrite').parquet(f"{PATH_OUT}/dim_{file_name}.parquet")
+    
 
 def prepare_airport_data():
-#%%
-    airport_data = os.path.join(S3_BUCKET_IN, AIRPORT_KEY)
-    df = spark.read.format('csv').options(header=True, delimiter=",").load(airport_data)
-    df.show()
+    """
+    Python function responsible to prepare the airport data
+    """
 
-#%%
+    airport_schema = StructType([
+        StructField('ident', StringType()),
+        StructField('type', StringType()),
+        StructField('name', StringType()),
+        StructField('elevation_ft', IntegerType()),
+        StructField('continent', StringType()),
+        StructField('iso_country', StringType()),
+        StructField('iso_region', StringType()),
+        StructField('municipality', StringType()),
+        StructField('iata_code', StringType()),
+        StructField('coordinates', StringType())
+    ])
+    
+    airport_data = AIRPORT_KEY
+    df_airport = spark.read.format('csv').options(header=True, delimiter=",").schema(airport_schema).load(airport_data)
+    
+    df_airport_uni = df_airport.dropDuplicates(["ident"])
+    df_airport_new_columns = df_airport_uni \
+        .withColumn('latitude', split(df_airport_uni['coordinates'], ',').getItem(0)) \
+        .withColumn('longitude', split(df_airport_uni['coordinates'], ',').getItem(1)) \
+        .withColumn('country', lit('United States'))
+    df_dropped = df_airport_new_columns.drop('coordinates')
+    
+    file_name = airport_data.split("/")[-1].split('.')[0]
+    df_dropped.write.mode('overwrite').parquet(f"{PATH_OUT}/dim_{file_name}.parquet")
+
+def prepare_immigration_data():
+    """
+    Python function responsible to prepare the immigration data
+    """
+    immigration_data = IMMIGRATION_KEY
+    df_imi = spark.read.format('com.github.saurfang.sas.spark').load(immigration_data)
+    
+    df_imi_uni = df_imi.dropDuplicates(["cicid"])
+    df_imi_new = df_imi_uni.withColumn('country', lit('United States'))
+    
+    file_name = immigration_data.split("/")[-2]
+    df_imi_new.write.mode('overwrite').parquet(f"{PATH_OUT}/fact_{file_name}.parquet")
+
+def prepare_temperatures_data():
+    """
+    Python function responsible to prepare the temperatures data
+    """
+
+    temperature_schema = StructType([
+        StructField('dt', DateType()),
+        StructField('AverageTemperature', DoubleType()),
+        StructField('AverageTemperatureUncertainty', DoubleType()),
+        StructField('City', StringType()),
+        StructField('Latitude', StringType()),
+        StructField('Longitude', StringType())
+    ])
+
+    temperatures_data = TEMPERATURES_KEY
+    df_temperatures = spark.read.format('csv').options(header=True, delimiter=",").schema(temperature_schema).load(temperatures_data)
+    
+    df_temperatures_uni = df_temperatures.dropDuplicates(["dt", 'City', 'Country'])
+    df_temperatures_renamed = df_temperatures_uni.withColumnRenamed('AverageTemperature', 'average_temperature')\
+        .withColumnRenamed('AverageTemperatureUncertainty', 'average_temperature_uncertainty')\
+        .withColumnRenamed('City', 'city')\
+        .withColumnRenamed('Country', 'country')\
+        .withColumnRenamed('Latitude', 'latitude')\
+        .withColumnRenamed('Longitude', 'longitude')\
+        .withColumn('id_temp', monotonically_increasing_id())
+
+    file_name = temperatures_data.split("/")[-1].split('.')[0]
+    df_temperatures_renamed.write.mode('overwrite').parquet(f"{PATH_OUT}/dim_{file_name}.parquet")
+
 def create_spark_session():
     """
     This function creates a configured PySpark Session to work with Spark in Python
@@ -53,20 +154,16 @@ def create_spark_session():
     Returns:
         SparkSession: Return a Spark Session object able to work throughout the code.
     """
-
-    spark = SparkSession \
-        .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0") \
-        .config("fs.s3a.access.key", ACCESS_KEY) \
-        .config("fs.s3a.secret.key", SECRET_KEY) \
-        .enableHiveSupport() \
-        .getOrCreate()
-    return spark
+    return SparkSession.builder.\
+        config("spark.jars.repositories", "https://repos.spark-packages.org/").\
+        config("spark.jars.packages", "saurfang:spark-sas7bdat:2.0.0-s_2.11").getOrCreate()
 
 
 if __name__ == "__main__":
     spark = create_spark_session()
-    #prepare_demographic_data()
+    
+    prepare_demographic_data()
     prepare_airport_data()
+    prepare_immigration_data()
+    prepare_temperatures_data()
 
-# %%
